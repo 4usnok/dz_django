7e3a86379bb3c39b88b803e782de8cf53f35ee6f
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-06-11 02:55:14 +0300

    exercises 3

--- catalog/templatetags/my_tags.py
+++ catalog/templatetags/my_tags.py
@@ -6,5 +6,5 @@ register = template.Library()
 @register.filter()
 def media_filter(path):
     if path:
-        return f"media/{path}"
+        return path.url # встроенный метод .url для получения полного пути
     return "#"

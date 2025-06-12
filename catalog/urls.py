eafaacefd2678bc5ca24b82378db68c9472bc378
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-06-13 02:27:56 +0300

    fix

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 
-from . import views
+from catalog.views import home, contact
 
 app_name = "catalog"
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
-    path("contacts/", views.contacts, name="contacts"),
-    path("products_info/", views.product_info, name="product_info"),
-    path("products_info/<int:pk>/", views.product_detail, name="product_detail")
+    path("home/", home, name="home"),
+    path("home/<int:pk>/", contact, name="contact")
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -5,23 +5,11 @@ from catalog.models import Product
 
 
 def home(request):
-    return render(request, "catalog/home.html")
-
-
-def contacts(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        message = request.POST.get("message")
-
-        return HttpResponse(f"Спасибо, {name}! Сообщение получено.")
-    return render(request, "catalog/contacts.html")
-
-def product_info(request):
     products = Product.objects.all()
     context = {"products": products}
-    return render(request,"catalog/prod_info.html", context)
+    return render(request, "catalog/product_list.html", context)
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
+def contact(request, pk):
+    product = Product.objects.get(pk=pk)
     context = {"product": product}
-    return render(request, "catalog/products_detail.html", context)
+    return render(request, "catalog/contacts.html", context)

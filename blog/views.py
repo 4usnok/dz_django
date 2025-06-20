eda9e6682c9594a493e799f0d6e09269cad90829
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-06-20 03:07:24 +0300

    Exercises 3

--- blog/views.py
+++ blog/views.py
@@ -8,12 +8,18 @@ class BlogListView(ListView):
     template_name = "blog/base_view.html"
     context_object_name = "posts"
 
+    def get_queryset(self):
+        """ Фильтрация опубликованных статей """
+        queryset = super().get_queryset()
+        return queryset.filter(publication_sign=True)
+
 class InfoDetailView(DetailView):
     model = BlogPost
     template_name = "blog/crud/post_detail.html"
     context_object_name = "info"
 
     def get_object(self, queryset=None):
+        """ Увеличение счетчика просмотров при открытии отдельной статьи """
         self.object = super().get_object(queryset)
         self.object.number_of_views += 1
         self.object.save()
@@ -31,7 +37,9 @@ class BlogUpdateView(UpdateView):
     model = BlogPost
     template_name = "blog/crud/update_post.html"
     fields = ("title", "content", "preview", "publication_sign", "number_of_views")
+
     def get_success_url(self):
+        """ Перенаправление после редактирования на просмотр этой статьи """
         return reverse(
             "blog:info_post", # Имя из path() в urls.py
             kwargs={"pk": self.object.pk}  # Параметры для подстановки
